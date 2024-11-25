b86224aea66db85bd8b8c44bb1a3c6afabf6ab93
Faisal Ahamed Rahad <155021870+rahad-dev24@users.noreply.github.com>
2024-11-25 12:47:25 +0600

    added responsive css of weding party

--- components/WeddingParty.tsx
+++ components/WeddingParty.tsx
@@ -5,23 +5,23 @@ const WeddingParty = () => {
   return (
     <section
       id="party"
-      className=" grid grid-cols-2 auto-cols-max content-center pt-36 gap-x-40 gap-y-20 items-start"
+      className=" grid grid-cols-1 xl:grid-cols-2 auto-cols-max content-center pt-36 gap-x-10 2xl:gap-x-40 gap-y-20 items-start"
     >
-      <div className="col-span-2 text-5xl font-poppinsRegular text-center text-primary tracking-[3px]">
+      <div className="col-span-1 xl:col-span-2 text-5xl font-poppinsRegular text-center text-primary tracking-[3px]">
         WEDDING PARTY
       </div>
       {cardData.map((card) => (
         <div
-          className="flex flex-col justify-center items-center gap-2 pt-5 w-[535px]"
+          className="flex flex-col justify-center items-center gap-2 pt-5 md:w-2/3 lg:w-[535px] m-auto"
           key={card.id}
         >
           <div>
             <Image src={card.image} alt={card.name} width={100} height={100} />
           </div>
-          <div className="text-3xl font-pinyon text-secondary ">
+          <div className="text-3xl font-pinyon text-secondary text-center">
             {card.title}
           </div>
-          <div className="text-4xl font-poppinsLight text-primary font-light tracking-[3px] py-4">
+          <div className="text-4xl font-poppinsLight text-primary font-light tracking-[3px] py-4 text-center">
             {card.name}
           </div>
           <div className="text-lg font-poppinsLight text-secondary  text-center pt-4">
